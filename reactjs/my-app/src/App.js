80e486138d8429631478d5b45602f9817c0dc0c1
Pratim-01 <pratimdas2000@gmail.com>
2024-09-11 21:33:18 +0530

    test-3

--- reactjs/my-app/src/App.js
+++ reactjs/my-app/src/App.js
@@ -1,20 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Intro/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-}
-function Intro(){
-  return(
-    <div>
-      <h1>Welcome to my Online Music Gallrey</h1>
-    </div>
-
-  )
 }
 
 export default App;
