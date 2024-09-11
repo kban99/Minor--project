08b9a37b5f27b442aa9f456594b4ab9fea4df9a9
Kaustav Banerjee <bankaustav99@gmail.com>
2024-09-11 20:38:25 +0530

    Ist changes

---
+++ reactjs/my-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro/>
+    </div>
+  );
+}
+function Intro(){
+  return(
+    <div>
+      <h1>Welcome to my Online Music Gallrey</h1>
+    </div>
+
+  )
+}
+
+export default App;
