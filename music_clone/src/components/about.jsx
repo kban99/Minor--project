9490203c6d77bb2cab8debf3bc0f8a24f8bd4c8a
AriGhosh <aritraghosh3172002@gmail.com>
2024-09-23 16:23:01 +0530

    clg work

---
+++ music_clone/src/components/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+
+function About() {
+  return (
+    <div>
+      <>
+      <Header />
+      <h1>Hello From About Page</h1>
+
+      </>
+    </div>
+  )
+}
+
+export default About
---
+++ music_clone/src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+
+function Home() {
+  return (
+    <div>
+      <>
+      <Header />
+      <h1>Hello From Home Page</h1>
+
+      </>
+    </div>
+  )
+}
+
+export default Home
