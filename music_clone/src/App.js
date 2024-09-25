6a8a7f089717a1477a571d0086f70bbcc261b7a2
Kaustav Banerjee <bankaustav99@gmail.com>
2024-09-25 11:20:59 +0530

    resetiing

--- music_clone/src/App.js
+++ music_clone/src/App.js
@@ -1,22 +1,11 @@
 
 import './App.css';
-import{BrowserRouter,Routes,Route} from 'react-router-dom'
-import About from './components/about';
-import Header from './components/header';
-import Home from './components/home';
-import Gallery from './components/gallery';
+
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Header />}></Route>
-        <Route exact path='about' element={<About />}></Route>
-        <Route exact path='home' element={<Home />}></Route>
-        <Route exact path='gallery' element={<Gallery />}></Route>
-      </Routes>
-    </BrowserRouter>
+    
       
     </div>
   );
